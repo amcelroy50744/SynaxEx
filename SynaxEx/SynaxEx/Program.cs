c0f2d32b95abf959c38a04145310abf83dda9e18
Andre McElroy <amcelroy50744@gmail.com>
2021-06-21 16:12:27 -0500

    intitial commit

---
+++ SynaxEx/SynaxEx/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynaxEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            string response = (answer < 9) ? $"{answer}is less then nine" : $"{answer} is equeal to or greater than nine";
+
+          
+
+
+        }
+    }
+}
